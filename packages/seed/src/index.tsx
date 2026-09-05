d7e3af76c0ccf64a1edd931468ecc7cee3b644dc
jonathan diego rdz <jon_eldiablo@hotmail.com>
2026-09-05 01:47:43 -0600

    feat(core): port dbl stylesheets to @dblimp/core and wire SCSS into seed
    - Port dbl-components/src/scss faithfully to packages/core/src/scss/ (icons,
      grid-container, panel-container, views, forms, navigation, tables, utilities)
    - Export ./styles and ./styles/utilities from @dblimp/core (raw SCSS, compiled
      by consumers)
    - Seed: compile dbl SCSS with a bootstrap functions/variables/maps/mixins
      context via src/styles/dbl.scss (adds sass devDep)
    - Icons now size via .icon-inline CSS (16px 1rem) instead of the size:32 hack;
      catalog grids back to auto-* colClasses identical to the dbl-seed reference

--- packages/seed/src/index.tsx
+++ packages/seed/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/dbl.scss";
 import "./index.css";
 
 import appCtrl, { appReady } from "./app-ctrl";
